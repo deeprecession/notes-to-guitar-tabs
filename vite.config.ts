96fc9c4c16c22abf0bc41e9cb1476737e2c92bed
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-05-12 17:26:33 +0300

    chore: add alias '@' to /src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import svgLoader from "vite-svg-loader"
+import { fileURLToPath, URL } from "node:url"
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader()],
+    resolve: {
+        alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
+    },
 })
