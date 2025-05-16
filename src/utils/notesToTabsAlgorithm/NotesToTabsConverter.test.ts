1486e54e9da1fab2b91bafb298fd29902f9026d7
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-05-16 13:23:50 +0300

    feat: add and test findAllTabsForChords

---
+++ src/utils/notesToTabsAlgorithm/NotesToTabsConverter.test.ts
@@ -0,0 +1,134 @@
+import { describe, expect, it } from "vitest"
+import { findAllTabsForChord, type Chord, type Tab } from "./NotesToTabsConverter"
+import { standardTuning } from "./GuitarTuning"
+import { computeFretboard } from "./GuitarFretboard"
+
+describe("find all tabs for chord", () => {
+    it("should have no solution for pitch outside fretboard", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["E0"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = []
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should find one possible solution for E2 pitch", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["E2"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [[0, null, null, null, null, null]]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should find one possible solution for E5 pitch", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["E5"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [[null, null, null, null, null, 12]]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should find three possible solution for F#4 pitch", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["F#4"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [
+            [null, null, null, null, null, 2],
+            [null, null, null, null, 7, null],
+            [null, null, null, 11, null, null],
+        ]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should have one solution for for E2 and E5 pitches", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["E2", "E5"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [
+            [0, null, null, null, null, 12],
+            [null, null, null, null, null, 12],
+            [0, null, null, null, null, null],
+        ]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should have 7 solutions for for E2 and F#4 pitches", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["F#4", "E2"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [
+            [0, null, null, null, null, null],
+            [0, null, null, 11, null, null],
+            [0, null, null, null, 7, null],
+            [0, null, null, null, null, 2],
+            [null, null, null, 11, null, null],
+            [null, null, null, null, 7, null],
+            [null, null, null, null, null, 2],
+        ]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should have 2 solutions for D5 and E5 pitches", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["D5", "E5"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        console.log(tabs)
+
+        const expected: Tab[] = [
+            [null, null, null, null, null, 10],
+            [null, null, null, null, null, 12],
+        ]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should have 2 solutions for D5 and E5 pitches", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["D5", "E5"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab[] = [
+            [null, null, null, null, null, 10],
+            [null, null, null, null, null, 12],
+        ]
+        expect(tabs).to.have.deep.members(expected)
+    })
+
+    it("should include solution for Am chord", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["A2", "E3", "A3", "C4", "E4"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        const expected: Tab = [null, 0, 2, 2, 1, 0]
+
+        expect(tabs).to.include.deep.includes(expected)
+    })
+
+    it("should include solution for Gm chord", () => {
+        const fretboard = computeFretboard(standardTuning)
+        const chord: Chord = ["G2", "D3", "G3", "A#3", "D4", "G4"]
+
+        const tabs = findAllTabsForChord(fretboard, chord)
+
+        console.log(tabs.length)
+
+        const expected: Tab = [3, 5, 5, 3, 3, 3]
+
+        expect(tabs).to.include.deep.includes(expected)
+    })
+})
---
+++ src/utils/notesToTabsAlgorithm/NotesToTabsConverter.ts
@@ -0,0 +1,74 @@
+import type { Fretboard } from "./GuitarFretboard"
+import type { Pitch } from "./Pitch"
+
+export type Tab = (number | null)[]
+
+export type Chord = Pitch[]
+// type ChordSequence = Chord[]
+//
+// type TabSolutionMetrics = {
+//     result: Tab
+//     firstFret: number
+//
+//     fretLength: number
+//     fretShift: number
+//     pitchesFailed: number
+// }
+//
+// export function convertChordSequence(fretboard: Fretboard, sequence: ChordSequence): Tab[] {
+//     return sequence.map((chord) => convertChord(fretboard, chord))
+// }
+//
+// function convertChord(fretboard: Fretboard, chord: Chord): Tab {
+//     return []
+// }
+
+export function findAllTabsForChord(fretboard: Fretboard, chord: Chord): Tab[] {
+    function helper(fretboard: Fretboard, chord: Chord, curSolution: Tab = []): Tab[] {
+        if (fretboard.length === 0) return [curSolution]
+
+        if (chord.length === 0) {
+            const curSolutionWithFilledLastStrings = [
+                [...curSolution, ...Array(fretboard.length).fill(null)],
+            ]
+            return curSolutionWithFilledLastStrings
+        }
+
+        const solutions: Tab[] = []
+
+        while (fretboard.length > 0) {
+            // take string
+            const string = fretboard[0]
+
+            // find a note from chord on string
+            for (let fretInx = 0; fretInx < string.length; fretInx++) {
+                const fretPitch = string[fretInx]
+
+                for (let chordInx = 0; chordInx < chord.length; chordInx++) {
+                    const chordPitch = chord[chordInx]
+
+                    if (chordPitch === fretPitch) {
+                        const nextSolution = [...curSolution, fretInx]
+                        const nextFretboard = fretboard.slice(1)
+                        const nextChord = [...chord]
+                        nextChord.splice(chordInx, 1)
+
+                        solutions.push(...helper(nextFretboard, nextChord, nextSolution))
+                    }
+                }
+            }
+
+            fretboard = fretboard.slice(1)
+            curSolution.push(null)
+        }
+
+        const isCusSolutionEmpty = curSolution.every((v) => v === null)
+        if (!isCusSolutionEmpty) {
+            solutions.push(curSolution)
+        }
+
+        return solutions
+    }
+
+    return helper(fretboard, chord)
+}
